befc67a0ef611f375eff98c0af1a6be7711499ad
imaru <imaru@neptune.kanazawa-it.ac.jp>
2023-05-01 10:49:24 +0900

    modify ttest and data for ttest

---
+++ make_week7_1.R
@@ -0,0 +1,33 @@
+# データ生成
+# 要約量
+mA<-65
+mB<-70
+mC<-70
+mD<-70
+sdA<-10
+sdB<-10
+sdC<-10
+sdD<-16
+
+numD<-20
+
+datA<-sort(rnorm(numD, mean=mA, sd=sdA))
+datB<-sort(rnorm(numD, mean=mB, sd=sdB))
+datC<-rnorm(numD, mean=mC, sd=sdC)
+datD<-rnorm(numD, mean=mD, sd=sdD)
+
+t.test(datA,datB)
+t.test(datA,datB,paired=T)
+t.test(datA,datD)
+
+dat<-data.frame(cbind(datA,datB,datC,datD))
+boxplot(dat)
+
+library(tidyr)
+library(ggpllot2)
+ldat<-tidyr::pivot_longer(dat, col=c('datA','datB','datC', 'datD'))
+
+ggplot(data=ldat,aes(x=name,y=value))+geom_violin()
+
+write.csv(dat,file='week7_1.csv',row.names = FALSE)                
+                
--- ttest.R
+++ ttest.R
@@ -1,41 +1,48 @@
 datA<-c(71,91,76,80,82)
 datB<-c(77,93,79,80,86)
+
+# csvファイルを読み込むときは上をコメントアウトして、下の3行を使う
+# 7,8行目は検定したいデータに合わせて変えること
+dat<-read.csv('week7_1.csv')
+datA<-dat$datA
+datB<-dat$datB
+
 tailed<-2 # 片側のときは1, 両側のときは2
 
 # 等分散の検定
-# sds<-c(sd(datA), sd(datB))
-# dfs<-c(length(datA)-1, length(datB)-1)
-# bb<-which(max(sds)==sds)
-# maxi<-which(max(sds)==sds)
-# mini<-which(min(sds)==sds)
-# Fr<-sds[maxi]^2/sds[mini]^2
-# Fp<-pf(Fr,dfs[maxi],dfs[mini], lower.tail = F)*2
-# print(paste('等分散の検定: F(', as.character(dfs[maxi]), ', ', as.character(dfs[mini]), ')=', as.character(round(Fr,3)), ', p=', as.character(round(Fp,3)),sep=''))
+sds<-c(sd(datA), sd(datB))
+dfs<-c(length(datA)-1, length(datB)-1)
+bb<-which(max(sds)==sds)
+maxi<-which(max(sds)==sds)
+mini<-which(min(sds)==sds)
+Fr<-sds[maxi]^2/sds[mini]^2
+Fp<-pf(Fr,dfs[maxi],dfs[mini], lower.tail = F)*2
+print(paste('等分散の検定: F(', as.character(dfs[maxi]), ', ', as.character(dfs[mini]), ')=', as.character(round(Fr,3)), ', p=', as.character(round(Fp,3)),sep=''))
 
 # 対応なし、分散が等しいことを仮定
 s_df<-length(datA)+length(datB)-2
 s_hsA<-sum((datA-mean(datA))^2)
 s_hsB<-sum((datB-mean(datB))^2)
 s_bunshi<-mean(datA)-mean(datB)
-s_bunbo1<-sqrt((s_hsA+s_hsB)/df)
+s_bunbo1<-sqrt((s_hsA+s_hsB)/s_df)
 s_bunbo2<-sqrt(1/length(datA)+1/length(datB))
 sT<-s_bunshi/(s_bunbo1*s_bunbo2)
 sp<-pt(abs(sT),s_df,lower.tail=FALSE)*tailed
-print(paste('対応なし・等分散：t(',as.character(df),')=',as.character(round(sT,3)),', p=',as.character(round(sp,3)),sep=''))
+print(paste('対応なし・等分散：t(',as.character(s_df),')=',as.character(round(sT,3)),', p=',as.character(round(sp,3)),sep=''))
 
 # 対応なし、分散が等しいことを仮定しない
-# w_bunshi<-mean(datA)-mean(datB)
-# w_bunbo<-sqrt(sd(datA)^2/length(datA)+sd(datB)^2/length(datB))
-# wT<-w_bunshi/w_bunbo
-# w_df_bunshi<-(sd(datA)^2/length(datA)+sd(datB)^2/length(datB))^2
-# w_df_bunbo_a<-sd(datA)^4/(length(datA)^2*(length(datA)-1))
-# w_df_bunbo_b<-sd(datB)^4/(length(datB)^2*(length(datB)-1))
-# w_df<-w_df_bunshi/(w_df_bunbo_a+w_df_bunbo_b)
-# wp<-pt(abs(wT),w_df,lower.tail=FALSE)*tailed
-# print(paste('対応なし：t(',as.character(round(w_df,3)),')=',as.character(round(wT,3)),', p=',as.character(round(wp,3)),sep=''))
+w_bunshi<-mean(datA)-mean(datB)
+w_bunbo<-sqrt(sd(datA)^2/length(datA)+sd(datB)^2/length(datB))
+wT<-w_bunshi/w_bunbo
+w_df_bunshi<-(sd(datA)^2/length(datA)+sd(datB)^2/length(datB))^2
+w_df_bunbo_a<-sd(datA)^4/(length(datA)^2*(length(datA)-1))
+w_df_bunbo_b<-sd(datB)^4/(length(datB)^2*(length(datB)-1))
+w_df<-w_df_bunshi/(w_df_bunbo_a+w_df_bunbo_b)
+wp<-pt(abs(wT),w_df,lower.tail=FALSE)*tailed
+print(paste('対応なし：t(',as.character(round(w_df,3)),')=',as.character(round(wT,3)),', p=',as.character(round(wp,3)),sep=''))
 
 # 対応あり
 diff<-datA-datB
 pT<-mean(diff)/sqrt(sd(diff)^2/length(diff))
-pp<-pt(abs(pairedT), length(diff)-1, lower.tail=FALSE)*tailed
+pp<-pt(abs(pT), length(diff)-1, lower.tail=FALSE)*tailed
 print(paste('対応あり：t(',as.character(length(diff)-1),')=',as.character(round(pT,3)),', p=',as.character(round(pp,3)),sep=''))
