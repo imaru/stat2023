ac71e71d197494f53500b7aa636f3307c9c5b3a0
imaru <imaruoka@gmail.com>
2023-05-18 18:09:44 +0900

    makeweek11

---
+++ makeWeek11.R
@@ -0,0 +1,24 @@
+library(tidyr)
+# データ生成
+# 要約量
+mA<-65
+mB<-67
+mC<-69
+sdA<-6
+sdB<-6
+sdC<-6
+
+numD<-20
+
+datA<-sort(rnorm(numD, mean=mA, sd=sdA))
+datB<-sort(rnorm(numD, mean=mB, sd=sdB))
+datC<-sort(rnorm(numD, mean=mC, sd=sdC))
+
+dat<-data.frame(cbind(datA,datB,datC))
+boxplot(dat)
+anovakun(dat,'sA',3)
+write.csv(dat,file='week11_1.csv', row.names = FALSE)
+
+dat<-tidyr::pivot_longer(dat,cols=c('datA','datB','datC'))
+anovakun(dat,'As',3)
+write.csv(dat,file='week10_1.csv', row.names = FALSE)
