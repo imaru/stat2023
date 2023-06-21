d78db7c6005c48e4e8c9fad42204107d10f68fd6
imaru <t_imaru@me.com>
2023-06-21 12:34:31 +0900

    week13 update2

--- make_week13.R
+++ make_week13.R
@@ -32,3 +32,31 @@ plot(math,phys)
 dat<-data.frame(cbind(math,eng,phys))
 write.csv(dat,file='week13_1.csv',row.names = FALSE) 
 
+# confidence interval
+n<-15
+mA<-60
+mB<-65
+
+sdA<-10
+sdB<-8
+
+methodA<-sort(rnorm(n, mean=mA, sd=sdA))
+methodB<-sort(rnorm(n, mean=mB, sd=sdB))
+hist(methodA)
+hist(methodB)
+
+lowerA<-mean(methodA)-sd(methodA)/sqrt(length(methodA))*qt(0.05/2,length(methodA)-1,lower.tail = F)
+upperA<-mean(methodA)+sd(methodA)/sqrt(length(methodA))*qt(0.05/2,length(methodA)-1,lower.tail = F)
+
+lowerB<-mean(methodB)-sd(methodB)/sqrt(length(methodB))*qt(0.05/2,length(methodB)-1,lower.tail = F)
+upperB<-mean(methodB)+sd(methodB)/sqrt(length(methodB))*qt(0.05/2,length(methodB)-1,lower.tail = F)
+
+print(c(lowerA, upperA))
+print(c(lowerB, upperB))
+
+t.test(methodA, methodB, paired=F, var.equal = T)
+t.test(methodA, methodB, paired=T, var.equal = T)
+cdat<-data.frame(cbind(methodA,methodB))
+boxplot(cdat)
+write.csv(cdat,file='week13_2.csv',row.names = FALSE) 
+
